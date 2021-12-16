7518b6a4cc5941d6b43501f0c65a967036c14d68
Nariman Hayatdavoud <94092361+narimanhd@users.noreply.github.com>
2021-12-15 21:40:17 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x,D) {
+  ##B=inverse of matrix
+  B<-NULL
+  ## 
+  intro<-function() A {
+    x<<-A
+    B<<-NULL
+  }
+  grab<-function() x
+  ## calcinv= calculates the inverse of matrix
+  calcinv<-function() B<<-solve(x)
+  ## showinv= inverse of matrix
+  showinv<-function B
+  
+ list(intro=intro,grab=grab,calcinv=calcinv,showinv=showinv)
+ 
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+####################################################
+cacheSolve <- function(x,...){
+    B<-x$showinv()
+  if(!is.null(B)){
+      return(B)
+    }
+    X <- x$intro()
+    inv <- solve(X, ...)
+    x$calcinv(B)
+    B
+    
 }
